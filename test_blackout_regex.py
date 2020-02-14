1574d43fe61ad9e1a2844e661a680c4f5f211988
Mircea Ulinic <mulinic@digitalocean.com>
2020-02-14 10:25:06 +0000

    Almost there

--- test_blackout_regex.py
+++ test_blackout_regex.py
@@ -3,9 +3,6 @@ import sys
 import logging
 import unittest
 
-from alertaclient.models.alert import Alert
-from alertaclient.models.blackout import Blackout
-
 from mock import Mock
 
 client_mod = sys.modules['alertaclient.api'] = Mock()
@@ -14,16 +11,48 @@ client_mod.Client = Mock()
 plugins_mod = sys.modules['alerta.plugins'] = Mock()
 plugins_mod.PluginBase = Mock
 
+
+class Model:
+    def __init__(self, **kwargs):
+        for arg, val in kwargs.items():
+            setattr(self, arg, val)
+
+
+class Blackout(Model):
+    pass
+
+
+class Alert(Model):
+    def tag(self, tags):
+        self.tags.extend(tags)
+
+    def untag(self, tags):
+        for tag in tags:
+            if tag in self.tags:
+                self.tags.remove(tag)
+
+    def set_status(self, status):
+        self.status = status
+
+
 client_mod.Client.return_value.get_blackouts.return_value = [
     Blackout(
         environment='test',
         resource='test\d',
+        service=None,
+        event=None,
+        group=None,
+        tags=[],
         duration=3600,
         id='1'
     ),
     Blackout(
         environment='test',
         service='service-([a-zA-Z])',
+        resource=None,
+        event=None,
+        group=None,
+        tags=[],
         duration=3600,
         id='2',
     ),
@@ -32,6 +61,9 @@ client_mod.Client.return_value.get_blackouts.return_value = [
         tags=[
             'site=site.*'
         ],
+        resource=None,
+        event=None,
+        group=None,
         duration=3600,
         id='3',
     ),
@@ -41,11 +73,26 @@ client_mod.Client.return_value.get_blackouts.return_value = [
             'site=site.*',
             'role=router'
         ],
+        resource=None,
+        event=None,
+        group=None,
         duration=3600,
         id='4'
+    ),
+    Blackout(
+        status='closed',
+        environment='test',
+        tags=[
+            'site=site.*',
+            'role=router'
+        ],
+        resource=None,
+        event=None,
+        group=None,
+        duration=3600,
+        id='5'
     )
 ]
-
 import blackout_regex
 from blackout_regex import BlackoutRegex
 
@@ -62,7 +109,9 @@ class TestEnhance(unittest.TestCase):
             id='no-match',
             resource='test::resource',
             event='test-event',
+            group='test',
             service=['test-service'],
+            tags=[],
             status='open',
         )
         test_obj = BlackoutRegex()
@@ -78,7 +127,9 @@ class TestEnhance(unittest.TestCase):
             id='match-resource',
             resource='test1',
             event='test-event',
+            group='test',
             service=['test-service'],
+            tags=[],
             status='open',
         )
         test_obj = BlackoutRegex()
@@ -94,7 +145,9 @@ class TestEnhance(unittest.TestCase):
             id='match-service',
             resource='test',
             event='test-event',
+            group='test',
             service=['service-blah'],
+            tags=[],
             status='open',
         )
         test_obj = BlackoutRegex()
@@ -110,6 +163,7 @@ class TestEnhance(unittest.TestCase):
             id='match-tag',
             resource='test',
             event='test-event',
+            group='test',
             service=['test-service'],
             tags=['site=siteX'],
             status='open',
@@ -117,7 +171,7 @@ class TestEnhance(unittest.TestCase):
         test_obj = BlackoutRegex()
         test = test_obj.post_receive(alert)
         self.assertEqual(test.status, 'blackout')
-        self.assertEqual(test.tags, ['regex_blackout=3'])
+        self.assertEqual(test.tags, ['site=siteX', 'regex_blackout=3'])
 
     def test_new_alert_no_match_tags(self):
         '''
@@ -127,6 +181,7 @@ class TestEnhance(unittest.TestCase):
             id='no-match-tags',
             resource='test',
             event='test-event',
+            group='test',
             service=['test-service'],
             tags=['site=siteX', 'role=switch'],
             status='open',
@@ -135,3 +190,39 @@ class TestEnhance(unittest.TestCase):
         test = test_obj.post_receive(alert)
         self.assertEqual(test.status, 'open')
         self.assertEqual(test.tags, [])
+
+    def test_old_alert_inactive_blackout(self):
+        '''
+        Test an existing alert whose associated blackout is currently inactive.
+        '''
+        alert = Alert(
+            id='old-alert',
+            resource='test',
+            event='test-event',
+            group='test',
+            service=['test-service'],
+            tags=['site=siteX', 'regex_blackout=5'],
+            status='open',
+        )
+        test_obj = BlackoutRegex()
+        test = test_obj.post_receive(alert)
+        self.assertEqual(test.status, 'open')
+        self.assertEqual(test.tags, ['site=siteX'])
+
+    def test_old_alert_removed_blackout(self):
+        '''
+        Test an existing alert whose associated blackout no longer exists.
+        '''
+        alert = Alert(
+            id='old-alert',
+            resource='test',
+            event='test-event',
+            group='test',
+            service=['test-service'],
+            tags=['site=siteX', 'regex_blackout=99'],
+            status='open',
+        )
+        test_obj = BlackoutRegex()
+        test = test_obj.post_receive(alert)
+        self.assertEqual(test.status, 'open')
+        self.assertEqual(test.tags, ['site=siteX'])
